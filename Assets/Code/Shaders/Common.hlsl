1e1a0f5e21d50f38322d8cbe2a13b4ac07115b14
Bartu Baş <bartubas99@gmail.com>
2024-03-08 08:40:28 +0300

    Prep for using instancing

--- Assets/Code/Shaders/Common.hlsl
+++ Assets/Code/Shaders/Common.hlsl
@@ -12,6 +12,7 @@
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
 #include "UnityInput.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/UnityInstancing.hlsl"
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
 
 #endif
