b6621a17e9f672c529e801662e3d413eb3db28d7
Bartu Baş <bartubas99@gmail.com>
2024-03-08 08:50:27 +0300

    Instanced Drawing shader

--- Assets/Code/Shaders/UnlitPass.hlsl
+++ Assets/Code/Shaders/UnlitPass.hlsl
@@ -4,23 +4,38 @@
 
 #include "Common.hlsl"
 
+// Vertex Shader Input
 struct Attributes
 {
     float3 objectSpacePosition : POSITION;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+// Vertex Shader Output, Fragment Shader Input
+// Varyings: Unity Naming, The Data can vary between fragments of the same triangle.
+struct Varyings
+{
+    float4 clipSpacePosition : SV_POSITION;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 // Makes it SRP Batcher Compatible.
-CBUFFER_START(UnityPerMaterial)
-    float4 _BaseColor;
-CBUFFER_END
+UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
-float4 UnlitPassVertex(Attributes input) : SV_POSITION
+Varyings UnlitPassVertex(Attributes input)
 {
-    const float3 positionWS = TransformObjectToWorld(input.objectSpacePosition.xyz);
-    return TransformWorldToHClip(positionWS);
+    Varyings output;
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_TRANSFER_INSTANCE_ID(input, output);
+    const float3 worldSpacePosition = TransformObjectToWorld(input.objectSpacePosition.xyz);
+    output.clipSpacePosition = TransformWorldToHClip(worldSpacePosition);
+    return output;
 }
 
-float4 UnlitPassFragment() : SV_TARGET
+float4 UnlitPassFragment(Varyings input) : SV_TARGET
 {
-    return _BaseColor;
+    UNITY_SETUP_INSTANCE_ID(input);
+    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
 }
