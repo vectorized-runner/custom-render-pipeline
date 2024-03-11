24d0ac3e24ad96379eb070d2e9ee88c883c56793
Bartu Baş <bartubas99@gmail.com>
2024-03-11 09:01:26 +0300

    Drawing

--- Assets/Code/Editor/CustomShaderGUI.cs
+++ Assets/Code/Editor/CustomShaderGUI.cs
@@ -47,12 +47,20 @@ namespace CustomSRP.Editor
 			}
 		}
 
+		private bool HasPremultiplyAlpha => TryGetProperty("_PremulAlpha", out _);
+
 		public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
 		{
 			base.OnGUI(materialEditor, properties);
 			_properties = properties;
 			_editor = materialEditor;
 			_materials = materialEditor.targets.Select(o => o as Material).ToArray();
+
+			OpaquePreset();
+			ClipPreset();
+			FadePreset();
+			TransparentPreset();
+		}
 		}
 
 		private void SetProperty(string name, float value)
@@ -81,6 +89,69 @@ namespace CustomSRP.Editor
 				{
 					material.DisableKeyword(keyword);
 				}
+			}
+		}
+
+		private bool PresetButton(string name)
+		{
+			if (GUILayout.Button(name))
+			{
+				_editor.RegisterPropertyChangeUndo(name);
+				return true;
+			}
+
+			return false;
+		}
+
+		private void OpaquePreset()
+		{
+			if (PresetButton("Opaque"))
+			{
+				Clipping = false;
+				PremultiplyAlpha = false;
+				SrcBlend = BlendMode.One;
+				DstBlend = BlendMode.Zero;
+				ZWrite = true;
+				RenderQueue = RenderQueue.Geometry;
+			}
+		}
+
+		private void ClipPreset()
+		{
+			if (PresetButton("Clip"))
+			{
+				Clipping = true;
+				PremultiplyAlpha = false;
+				SrcBlend = BlendMode.One;
+				DstBlend = BlendMode.Zero;
+				ZWrite = true;
+				RenderQueue = RenderQueue.AlphaTest;
+			}
+		}
+
+		private void FadePreset()
+		{
+			if (PresetButton("Fade"))
+			{
+				Clipping = false;
+				PremultiplyAlpha = false;
+				SrcBlend = BlendMode.SrcAlpha;
+				DstBlend = BlendMode.OneMinusSrcAlpha;
+				ZWrite = false;
+				RenderQueue = RenderQueue.Transparent;
+			}
+		}
+
+		private void TransparentPreset()
+		{
+			if (HasPremultiplyAlpha && PresetButton("Transparent"))
+			{
+				Clipping = false;
+				PremultiplyAlpha = true;
+				SrcBlend = BlendMode.One;
+				DstBlend = BlendMode.OneMinusSrcAlpha;
+				ZWrite = false;
+				RenderQueue = RenderQueue.Transparent;
 			}
 		}
 	}
