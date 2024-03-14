c26437fabdb32ba35eb4beddf37a1ec42e0bcecf
Bartu Baş <bartubas99@gmail.com>
2024-03-14 15:01:20 +0300

    Create ShadowSettings object

--- Assets/Code/SRPSettings.cs
+++ Assets/Code/SRPSettings.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine.Serialization;
 
 namespace CustomSRP
 {
@@ -8,5 +9,29 @@ namespace CustomSRP
 		public bool UseGPUInstancing;
 		public bool UseSRPBatcher;
 		public bool UseDynamicBatching;
+
+		public ShadowSettings ShadowSettings;
+	}
+
+	[Serializable]
+	public struct ShadowSettings
+	{
+		public float MaxDistance;
+	}
+
+	[Serializable]
+	public struct Directional
+	{
+		public TextureSize AtlasSize;
+	}
+
+	public enum TextureSize
+	{
+		_256 = 256,
+		_512 = 512,
+		_1024 = 1024,
+		_2048 = 2048,
+		_4096 = 4096,
+		_8192 = 8192
 	}
 }
